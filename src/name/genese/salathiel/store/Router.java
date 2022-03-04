2fa118856b20241ae8a85183052654f8cdace07f
Salathiel Genese <salathiel@genese.name>
2022-03-04 08:19:12 +0100

    feat: initialize router with CategoryController

--- src/name/genese/salathiel/store/Router.java
+++ src/name/genese/salathiel/store/Router.java
@@ -1,5 +1,9 @@
 package name.genese.salathiel.store;
 
+import name.genese.salathiel.store.controller.CategoryController;
+import name.genese.salathiel.store.repository.CategoryRepository;
+
+import java.sql.Connection;
 import java.util.Map;
 
 /**
@@ -14,6 +18,13 @@ public class Router {
     private static final String CATEGORIES_CREATE = "/categories/create";
     private static final String CATEGORIES_UPDATE = "/categories/update";
     private static final String CATEGORIES_DELETE = "/categories/delete";
+
+    private final CategoryController categoryController;
+
+    public Router(final Connection connection) {
+        final CategoryRepository categoryRepository = new CategoryRepository(connection);
+        categoryController = new CategoryController(categoryRepository);
+    }
 
     public boolean accepts(final String path, final Map<String, String> request) {
         switch (path) {
