315133bb945e2e88901caca94da4afda67bed486
Salathiel Genese <salathiel@genese.name>
2022-03-04 06:28:09 +0100

    feat: add input/output loop

--- src/name/genese/salathiel/store/Main.java
+++ src/name/genese/salathiel/store/Main.java
@@ -1,7 +1,27 @@
 package name.genese.salathiel.store;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final Router router = new Router();
+        Map<String, String> request;
+        String[] pair;
+        String path;
+        String line;
+
+        do {
+            request = new HashMap<>();
+            path = scanner.nextLine();
+
+            while (!(line = scanner.nextLine()).isBlank()) {
+                pair = line.split("=", 2);
+                request.put(pair[0], 1 == pair.length ? null : pair[1]);
+            }
+        } while (router.accepts(path, request));
     }
 }
