5d472b9ca2f12c643853e38ddb263f593db9ad52
Salathiel Genese <salathiel@genese.name>
2022-03-04 08:24:31 +0100

    fix: refactor change in Router constructor

--- src/name/genese/salathiel/store/Main.java
+++ src/name/genese/salathiel/store/Main.java
@@ -1,5 +1,8 @@
 package name.genese.salathiel.store;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -8,22 +11,30 @@ public class Main {
 
     // DROP TABLE IF EXISTS;
     // CREATE TABLE category(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) UNIQUE);
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        final Router router = new Router();
-        Map<String, String> request;
-        String[] pair;
-        String path;
-        String line;
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
 
-        do {
-            request = new HashMap<>();
-            path = scanner.nextLine();
+        final String PASSWORD = "";
+        final String USERNAME = "root";
+        final String URL = "jdbc:mysql://localhost:3306/store";
 
-            while (!(line = scanner.nextLine()).isBlank()) {
-                pair = line.split("=", 2);
-                request.put(pair[0], 1 == pair.length ? null : pair[1]);
-            }
-        } while (router.accepts(path, request));
+        try (final Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            final Scanner scanner = new Scanner(System.in);
+            final Router router = new Router(connection);
+            Map<String, String> request;
+            String[] pair;
+            String path;
+            String line;
+
+            do {
+                request = new HashMap<>();
+                path = scanner.nextLine();
+
+                while (!(line = scanner.nextLine()).isBlank()) {
+                    pair = line.split("=", 2);
+                    request.put(pair[0], 1 == pair.length ? null : pair[1]);
+                }
+            } while (router.accepts(path, request));
+        }
     }
 }
