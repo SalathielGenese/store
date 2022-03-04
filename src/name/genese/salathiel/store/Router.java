6b273e6c2f699a83961152a20ce57625cf80a524
Salathiel Genese <salathiel@genese.name>
2022-03-04 08:17:03 +0100

    feat: begin adding some more routes handlers to the router

--- src/name/genese/salathiel/store/Router.java
+++ src/name/genese/salathiel/store/Router.java
@@ -10,13 +10,28 @@ import java.util.Map;
  */
 public class Router {
     private static final String EXIT = "/exit";
+    private static final String CATEGORIES = "/categories";
+    private static final String CATEGORIES_CREATE = "/categories/create";
+    private static final String CATEGORIES_UPDATE = "/categories/update";
+    private static final String CATEGORIES_DELETE = "/categories/delete";
 
     public boolean accepts(final String path, final Map<String, String> request) {
         switch (path) {
             case EXIT:
                 return false;
+            case CATEGORIES:
+                final String terms = request.get("terms");
+                final String name = request.get("name");
+                final String id = request.get("id");
+
+                if (null == terms) {
+                    //
+                }
+                break;
             default:
                 throw new UnsupportedOperationException(String.format("PATH '%s'", path));
         }
+
+        return true;
     }
 }
