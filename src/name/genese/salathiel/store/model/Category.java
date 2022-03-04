3572bfe03b477aa29b8ff6eefbda03e9700d95dd
Salathiel Genese <salathiel@genese.name>
2022-03-04 07:16:25 +0100

    feat: model Category data structure

---
+++ src/name/genese/salathiel/store/model/Category.java
@@ -0,0 +1,38 @@
+package name.genese.salathiel.store.model;
+
+/**
+ * category
+ *
+ * @author Salathiel @t salathiel@genese.name
+ * @since Mar 04, 2022 @t 06:29:03
+ */
+public class Category {
+    private final int id;
+    private String name;
+
+    public Category(final String name) {
+        this(0, name);
+    }
+
+    public Category(final int id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Category{ id=%d, name='%s' }", id, name);
+    }
+}
