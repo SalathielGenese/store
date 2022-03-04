055284adc0e52ef54ef4863125d1204a47acc962
Salathiel Genese <salathiel@genese.name>
2022-03-04 08:11:14 +0100

    fix: extract path as class static field

--- src/name/genese/salathiel/store/Router.java
+++ src/name/genese/salathiel/store/Router.java
@@ -9,9 +9,11 @@ import java.util.Map;
  * @since Mar 04, 2022 @t 06:06:42
  */
 public class Router {
+    private static final String EXIT = "/exit";
+
     public boolean accepts(final String path, final Map<String, String> request) {
         switch (path) {
-            case "/exit":
+            case EXIT:
                 return false;
             default:
                 throw new UnsupportedOperationException(String.format("PATH '%s'", path));
